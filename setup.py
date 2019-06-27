f10d4bba585bee9c4ef8203f452940ca407484d4
gayathri1972 <52233526+gayathri1972@users.noreply.github.com>
2019-06-27 14:24:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,123 @@
-#/usr/bin/env python
-import io
-import re
-from setuptools import setup
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<queue>
+#include<stack>
+#include<math.h>
+#include<algorithm>
+#include<string>
+using namespace std;
+#define whatis(x) cout<<#x<<" : "<<x<<endl
 
+int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int hashfn[10] = {} ;
+int months(int i) {
+    int temp = i;
+        int l  = temp % 10;
+        temp = temp / 10;
+        if( ( l != temp ) && hashfn[l]>0 && hashfn[temp] > 0)  {
+            hashfn[l]--;
+            hashfn[temp]--;
+            return i;
+        }
+        else if( l == temp && hashfn[l]>1 ) {
+                hashfn[l]--;
+                hashfn[l]--;
+                return i;
+        }
+return -1;
+}
+vector<int> switch2pre (vector<int> &upd) {
+    vector<int> upd2(10, 0);
+    for(int i = 0; i<10; i++) {
+        hashfn[i] = upd[i];
+        upd2[i] = upd[i];
+    }
+    return upd2;
+}
+vector<int> updated () {
+    vector<int> upd2(10, 0);
+    for(int i = 0; i<10; i++) {
 
-with io.open('./seasnake/__init__.py', encoding='utf8') as version_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
-    if version_match:
-        version = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
+        upd2[i] = hashfn[i];
+    }
+    return upd2;
 
+}
+int main() {
 
-with io.open('README.rst', encoding='utf8') as readme:
-    long_description = readme.read()
+    vector<int> upd(10, 0);
+    for(int i = 0; i<12; i++) {
+        int d;
+        char ch;
+        cin>>d;
+        if(i != 11)
+        cin>>ch;
+        hashfn[d]++;
+        upd[d]++;
+    }
 
+    //initialised
+     int mm = -1, dd = -1, hh = -1, ss = -1;
+    for(int i = 12; i>0; i--) {
+        //For MONTHS
+        vector<int> upd2 = switch2pre(upd);
 
-setup(
-    name='seasnake',
-    version=version,
-    description='A tool to manage conversion of C++ code to Python.',
-    long_description=long_description,
-    author='Russell Keith-Magee',
-    author_email='russell@keith-magee.com',
-    url='http://pybee.org/seasnake',
-    packages=[
-        'seasnake',
-    ],
-    install_requires=[
-        'sealang'
-    ],
-    entry_points={
-        'console_scripts': [
-            'seasnake = seasnake.__main__:main',
-        ]
-    },
-    license='New BSD',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development',
-        'Topic :: Utilities',
-    ],
-    test_suite='tests'
-)
+        mm = months(i);
+
+        if(mm != -1) {
+
+            upd2 = updated();
+
+            for(int j = days[i-1]; j>0; j--) {
+
+                vector<int> upd3 = switch2pre(upd2);
+
+                dd = months(j);
+
+                if(dd!=-1)
+                    {
+
+                    upd3 = updated();
+
+                    for(int k =23; k>=0; k--)
+                        {
+
+                        vector<int> upd4 = switch2pre(upd3);
+
+                        hh = months(k);
+
+                        if(hh != -1) {
+
+                            upd4 = updated();
+
+                               for(int z = 59; z>=0; z-- )
+                                {
+
+                              vector<int> upd5 = switch2pre(upd4);
+
+                            ss = months(z);
+
+                        if(ss != -1) break;
+                        }
+                    if(ss!=-1)
+                    break;
+                    }
+
+                    }
+            if(ss!=-1)
+            break;
+                }
+            }
+        if(ss!=-1)
+        break;
+        }
+    }
+    if( mm == -1 || ss == -1 || dd == -1 | hh == -1)
+        cout<<0;
+    else
+
+    cout << mm<< "/" << dd<< "/" << hh << " :" <<  ss ;
+
+    return 0;
+}
